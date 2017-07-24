3ccec2819e541f23614b243bd268d299feafa5f9
pkuhner <pkuhner@users.noreply.github.com>
2017-07-24 08:12:13 +0200

    Add admin action to mark selected devices as approved

--- devices/admin.py
+++ devices/admin.py
@@ -19,6 +19,19 @@ class DeviceAdmin(admin.ModelAdmin):
     list_filter = ('approved', 'category')
     search_fields = ('name', 'mac_vendor')
     readonly_fields = ('uuid', 'thumbnail', 'image_url',)
+    actions = ['make_approved']
+
+    def make_approved(self, request, queryset):
+        rows_updated = queryset.update(approved=1)
+
+        if rows_updated == 1:
+            message_bit = "1 device was"
+        else:
+            message_bit = "%s devices were" % rows_updated
+
+        self.message_user(request, "%s successfully marked as approved." % message_bit)
+
+    make_approved.short_description = "Mark selected devices as approved"
 
 
 admin.site.site_title = settings.ADMIN_NAME
